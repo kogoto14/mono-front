8e0f983f9f7dfb27b4e601b3778977c8f9e9f81f
kogoto14 <oneokrock.tennis@gmail.com>
2025-09-02 22:00:26 +0900

    Introduction

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,22 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+
+<script lang="ts">
+    import Nested from './Nested.svelte'; 
+    let name = 'Svelte';
+    let src = '/tutorial/image.gif';
+    let string = 'this string contains some <strong>HTML!!!</strong>'
+</script>
+
+<h1>Hello {name.toUpperCase()}!</h1>
+<img {src} alt="{name} dances." />
+<p>This is a paragraph.</p>
+<Nested />
+<p>{@html string}</p>
+
+<style>
+    p {
+        color: goldenrod;
+        front-family: 'Comic Sans MS', cursive;
+        font-size: 2em;
+    }
+</style>
